6847e109e4777ab55d804a260f45d38f2b940d28
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-31 15:22:09 -0300

    Initial commit

---
+++ src/main/java/com/hbsmoura/springaopcourse/SpringAopCourseApplication.java
@@ -0,0 +1,13 @@
+package com.hbsmoura.springaopcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbsmoura/springaopcourse/SpringAopCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbsmoura.springaopcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
